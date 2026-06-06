5aec87defe8fe2f7d6b22b919da006c62df8b9e0
NifemiSoneye <nifemisoneye@gmail.com>
2026-06-06 11:27:53 -0700

    Base pages and routing set up

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,46 @@ import { lazy, Suspense } from "react";
 import { LoaderCircle } from "lucide-react";
 import PersistLogin from "./features/auth/PersistLogin";
 import RequireAuth from "./features/auth/RequireAuth";
+import RequireAdmin from "./features/auth/RequireAdmin";
 const Login = lazy(() => import("./pages/Login"));
+const Register = lazy(() => import("./pages/Register"));
+const CoursePage = lazy(() => import("./pages/CoursePage"));
+const CourseDetailsPage = lazy(() => import("./pages/CourseDetailsPage"));
+const VideoLessonPage = lazy(() => import("./pages/VideoLessonPage"));
+const AdminDashBoard = lazy(() => import("./pages/AdminDashBoard"));
+const Landing = lazy(() => import("./pages/Landing"));
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <Suspense
+        fallback={
+          <div className="fixed inset-0 bg-[#0B1628] grid place-content-center">
+            <LoaderCircle className="h-16 w-16 animate-spin text-white" />
+          </div>
+        }
+      >
+        <Routes>
+          <Route path="/" element={<Landing />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route element={<PersistLogin />}>
+            <Route element={<RequireAuth />}>
+              <Route path="/courses" element={<CoursePage />} />
+              <Route path="/courses/:id" element={<CourseDetailsPage />} />
+              <Route
+                path="/courses/:id/lessons/:lessonId"
+                element={<VideoLessonPage />}
+              />
+              <Route element={<RequireAdmin />}>
+                <Route path="/admin" element={<AdminDashBoard />} />
+              </Route>
+            </Route>
+          </Route>
+        </Routes>
+      </Suspense>
+    </>
+  );
 }
 
 export default App;
---
+++ src/features/courses/courseApiSlice.ts
@@ -0,0 +1,3 @@
+import { createSelector, createEntityAdapter } from "@reduxjs/toolkit";
+import { apiSlice } from "@/api/apiSlice";
+import { type RootState } from "../../app/store";
